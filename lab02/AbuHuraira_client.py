1b02cd34aef4bd4a6abe3948f3a10fc6811a9724
Abu Huraira <dear.huraira@gmail.com>
2023-04-06 20:21:28 +0300

    Complete lab 02

--- lab02/AbuHuraira_client.py
+++ lab02/AbuHuraira_client.py
@@ -1,22 +1,28 @@
+# Author: Abu Huraira
+# Email: a.huraira@innopolis.university
+
+# Note: Since the creation of the GIF is CPU intensive, we will use multiple processes to create the GIF.
+# On the other hand, the downloading of the frames is I/O intensive, so we will use multiple threads to download the frames.
+
 import os
 import socket
 import time
-
+from multiprocessing import Pool
+from threading import Thread
 from PIL import Image
 
 SERVER_URL = '127.0.0.1:1234'
-FILE_NAME = 'NameSurname.gif'
+FILE_NAME = 'AbuHuraira.gif'
 CLIENT_BUFFER = 1024
 FRAME_COUNT = 5000
+NUM_PROCESSES = os.cpu_count()  # number of processes to use for GIF creation
+NUM_THREADS = 10  # number of threads to use for downloading frames
 
 
-def download_frames():
-    t0 = time.time()
-    if not os.path.exists('frames'):
-        os.mkdir('frames')
-    for i in range(FRAME_COUNT):
+def download_frames(start, end):
+    ip, port = SERVER_URL.split(':')
+    for i in range(start, end):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            ip, port = SERVER_URL.split(':')
             s.connect((ip, int(port)))
             image = b''
             while True:
@@ -26,19 +32,44 @@ def download_frames():
                 image += packet
             with open(f'frames/{i}.png', 'wb') as f:
                 f.write(image)
-    return time.time() - t0
 
 
-def create_gif():
+def create_gif(process_id):
     t0 = time.time()
     frames = []
-    for frame_id in range(FRAME_COUNT):
+    for frame_id in range(process_id, FRAME_COUNT, NUM_PROCESSES):
         frames.append(Image.open(f"frames/{frame_id}.png").convert("RGBA"))
-    frames[0].save(FILE_NAME, format="GIF",
+    frames[0].save(f"{process_id}.gif", format="GIF",
                    append_images=frames[1:], save_all=True, duration=500, loop=0)
     return time.time() - t0
 
 
 if __name__ == '__main__':
-    print(f"Frames download time: {download_frames()}")
-    print(f"GIF creation time: {create_gif()}")
+    print("Downloading frames...")
+    t0 = time.time()
+    if not os.path.exists('frames'):
+        os.mkdir('frames')
+    pool = Pool(processes=NUM_THREADS)
+    batch_size = FRAME_COUNT // NUM_THREADS
+    for i in range(NUM_THREADS):
+        start = i * batch_size
+        end = (i + 1) * batch_size
+        pool.apply_async(download_frames, args=(start, end))
+    pool.close()
+    pool.join()
+    print(f"Frames download time: {time.time() - t0:.2f}s")
+
+    print("Creating GIF...")
+    t0 = time.time()
+    pool = Pool(processes=NUM_PROCESSES)
+    for i in range(NUM_PROCESSES):
+        pool.apply_async(create_gif, args=(i,))
+    pool.close()
+    pool.join()
+    gif_files = [f"{i}.gif" for i in range(NUM_PROCESSES)]
+    with open(FILE_NAME, 'wb') as f:
+        for gif_file in gif_files:
+            with open(gif_file, 'rb') as g:
+                f.write(g.read())
+            os.remove(gif_file)
+    print(f"GIF creation time: {time.time() - t0:.2f}s")
--- lab02/AbuHuraira_server.py
+++ lab02/AbuHuraira_server.py
@@ -1,39 +1,53 @@
-import os
+# Author: Abu Huraira
+# Email: a.huraira@innopolis.university
+
+# Note: Since the creation of the GIF is CPU intensive, we will use multiple processes to create the GIF.
+# On the other hand, the downloading of the frames is I/O intensive, so we will use multiple threads to download the frames.
+
 import socket
 import threading
-import random
 from PIL import Image
+import io
+import random
 
-SERVER_IP = '127.0.0.1'
+SERVER_IP = '0.0.0.0'
 SERVER_PORT = 1234
 BUFFER_SIZE = 1024
 
-def handle_connection(client_socket, client_address):
-    print(f"New connection from {client_address}")
-    image = Image.new('RGB', (10, 10), color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-    with open('temp.png', 'wb') as f:
-        image.save(f, 'png')
-    with open('temp.png', 'rb') as f:
-        data = f.read(BUFFER_SIZE)
-        while data:
-            client_socket.send(data)
-            data = f.read(BUFFER_SIZE)
-    os.remove('temp.png')
-    client_socket.close()
-    print(f"Connection from {client_address} closed")
 
-def start_server():
+def handle_client(client_socket, client_addr):
+    print(f"Sending image to {client_addr}")
+    # Create a random 10x10 image
+    img = Image.new('RGB', (10, 10), color=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    # Convert the image to bytes
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format='PNG')
+    img_bytes = img_bytes.getvalue()
+    # Send the image to the client
+    client_socket.sendall(img_bytes)
+    # Close the client socket
+    client_socket.close()
+
+
+def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((SERVER_IP, SERVER_PORT))
     server_socket.listen()
-    print(f"Server listening on {SERVER_IP}:{SERVER_PORT}")
+
+    print(f"Listening on {SERVER_IP}:{SERVER_PORT}")
     try:
         while True:
-            client_socket, client_address = server_socket.accept()
-            threading.Thread(target=handle_connection, args=(client_socket, client_address)).start()
+            # Accept a client connection
+            client_socket, client_addr = server_socket.accept()
+            # Spawn a new thread to handle the connection
+            t = threading.Thread(target=handle_client, args=(client_socket, client_addr))
+            t.start()
     except KeyboardInterrupt:
-        print("Server stopped")
+        print("Shutting down server.")
+    finally:
+        server_socket.close()
+
 
 if __name__ == '__main__':
-    start_server()
+    server()
